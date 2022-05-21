baefff5023782838d930475c12ae8235c7341e3c
Fagner Fernandes <Fagner Fernandes@DESKTOP-ACNLJLV>
2022-05-21 17:39:21 -0300

    Bat Inofensivo

---
+++ Dadostxt2/Dados.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dadostxt2
+{
+    public class Dados
+    {
+        public void Criar(string path)
+        {
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.FileName = path;
+            StreamWriter sw = new StreamWriter(path);
+            sw.WriteLine("@echo off");
+            sw.WriteLine("cls");
+            sw.WriteLine("@echo    *********************************************************");
+            sw.WriteLine("@echo    ***  Voce foi hackeado Precione Enter para continuar  ***");
+            sw.WriteLine("@echo    *********************************************************");
+            sw.WriteLine("pause");
+            sw.WriteLine("calc");
+            sw.WriteLine("calc");
+            sw.WriteLine("explorer");
+            sw.WriteLine("notepad");
+            sw.WriteLine("charmap");
+            sw.WriteLine("mspaint");
+            sw.WriteLine("cmd");
+            sw.WriteLine("taskmgr");
+            sw.Close();
+            System.Diagnostics.Process.Start(startInfo);
+        }
+        public void Abrir(string path)
+        {
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.FileName = path;
+            System.Diagnostics.Process.Start(startInfo);
+        }
+        public void CriaPasta(string patch)
+        {
+            Directory.CreateDirectory(@"C:\Temp\");
+        }
+    }
+}
---
+++ Dadostxt2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Security;
+using System.Windows;
+
+namespace Dadostxt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string path = @"C:\Temp\virus.bat";
+            Dados dados = new Dados();
+            if (!File.Exists(@"C:\Temp\"))
+            {
+                dados.CriaPasta(@"C:\Temp\");
+            }
+            if (!File.Exists(path))
+            {
+                dados.Criar(path);
+            }
+            else
+            {
+                dados.Abrir(path);
+            }
+        }
+    }
+}
